8702027951193c21e42000ec02e47d694b8ccf32
dimitur.trz <dimitar.trz@gmail.com>
2015-03-03 01:01:17 +0200

    1. Added the confirmable module in devise with a migration (run rake db:migrate) 2. Some changes in the config on those mails the bussiness with which is hairy to say the least - the config should now send from development if you put in a mail account username and password in the config/environments/development.rb(dont forget to remove it afterwards as youll be pushing to git). Youll also have to lower your mail security(https://www.google.com/settings/security/lesssecureapps) so that the server can log in and send its mail. 3. The validation is usable but quite inefficient on such an early stage

--- quizzes1/db/schema.rb
+++ quizzes1/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302195408) do
+ActiveRecord::Schema.define(version: 20150302211650) do
 
   create_table "answers", primary_key: "answer_id", force: :cascade do |t|
     t.integer "question_id", limit: 4
@@ -58,8 +58,12 @@ ActiveRecord::Schema.define(version: 20150302195408) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
