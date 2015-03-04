aa8a0c824c09d00149cde55cd298ff6f2555cda9
stamaniorec <stamaniorec@gmail.com>
2015-03-04 18:10:13 +0200

    Added dependent: :destroy to the Quiz model because when I'd try to delete a quiz, I'd get a "foreign key constraint fails" error by mysql Also added a link to the home page from the user profile page.

--- quizzes1/app/models/quiz.rb
+++ quizzes1/app/models/quiz.rb
@@ -2,6 +2,6 @@ class Quiz < ActiveRecord::Base
   has_many :questions, dependent: :destroy
   accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
 
-  has_many :scores
+  has_many :scores, dependent: :destroy
   belongs_to :user
 end
