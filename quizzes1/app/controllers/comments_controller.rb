46384b3cb2afaa0743b40ec30a370213901461d4
stamaniorec <stamaniorec@gmail.com>
2015-03-07 14:14:05 +0200

    Added presence validation for comment body Added some notices Cleaned up the comments controller (nothing important, small fixes)

--- quizzes1/app/controllers/comments_controller.rb
+++ quizzes1/app/controllers/comments_controller.rb
@@ -3,15 +3,11 @@ class CommentsController < ApplicationController
     @quiz = Quiz.find(params[:quiz_id])
     @comment = @quiz.comments.new(comment_params)
     @comment.user = current_user
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @quiz, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    
+    if @comment.save
+      redirect_to @quiz, notice: 'Comment was successfully created.'
+    else
+      redirect_to @quiz, notice: 'Comment is empty, cannot post.'
     end
   end
 
@@ -22,23 +18,17 @@ class CommentsController < ApplicationController
 
   def update
     @comment = Comment.find(params[:id])
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to quiz_path(@comment.quiz), notice: 'Question was successfully updated.' }
-        format.json { render :show, status: :ok, location: @question }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    
+    if @comment.update(comment_params)
+      redirect_to quiz_path(@comment.quiz), notice: 'Comment was successfully updated.'
+    else
+      redirect_to quiz_path(@comment.quiz), notice: 'Comment is empty, cannot update.'
     end
   end
 
   def destroy
     Comment.find(params[:id]).destroy
-    respond_to do |format|
-      format.html { redirect_to :back, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to :back, notice: 'Comment was successfully destroyed.'
   end
 
   private
