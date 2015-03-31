20136342b441d5c07e1657bb6bb06d8e426539ff
stamaniorec <stamaniorec@gmail.com>
2015-03-31 21:05:52 +0300

    Cleaned up and improved the search functionality.

--- quizzes1/app/controllers/search_controller.rb
+++ quizzes1/app/controllers/search_controller.rb
@@ -1,8 +1,17 @@
 class SearchController < ApplicationController
-	def index
-		
+  def index
+    
+  end
+  def show
+    @results = Quiz.all
+    if params[:quiz_search] && !params[:quiz_search].blank?
+      @results = @results.search_for params[:quiz_search]
+    end
+    if params[:user_search] && !params[:user_search].blank?
+	  @results = @results.search_for params[:user_search], order: 'email asc'
 	end
-	def show
-		
+    if params[:question_search] && !params[:question_search].blank?
+	  @results = @results.search_for params[:question_search]
 	end
+  end
 end
