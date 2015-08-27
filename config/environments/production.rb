0d7ea4b580aa224c095a2a6d0b1632736a84eb75
stamaniorec <stamaniorec@gmail.com>
2015-08-28 00:01:36 +0300

    production config for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'example.com' }
-	# ActionMailer Config
-	# Setup for production - deliveries, no errors raised
+  config.action_mailer.default_url_options = { host: 'peaceful-falls-2663.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",   
+    :user_name            => "development6337@gmail.com",
+    :password             => "password6337",
+    :authentication       => "plain",
+    :enable_starttls_auto => true  
+  }
 end
