a181815bdd0368aec0144b0191d15ef7f8f9a055
stamaniorec <stamaniorec@gmail.com>
2015-08-28 00:29:03 +0300

    sick of this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { host: 'peaceful-falls-2663.herokuapp.com', protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
